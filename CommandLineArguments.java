01fd1216df938b0790b14741d6bf8f4c252a5112
ram-15s <118020141+ram-15s@users.noreply.github.com>
2022-11-12 15:35:16 +0530

    Update CommandLineArguments.java

--- CommandLineArguments.java
+++ CommandLineArguments.java
@@ -1,7 +1,7 @@
 public class CommandLineArguments{
 	public static void main(String ramesh[]){
-			for(int i=0; i<ramesh.length; ++i){
-				System.out.printf("%s\n",ramesh[i]);
-			}
+		for(int i=0; i<ramesh.length; ++i){
+			System.out.printf("%s\n",ramesh[i]);
+		}
 	}
 }
